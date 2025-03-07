f1723aa90377072fb703aca06855c45051e865e0
Mateusz <ec2200386@edinburghcollege.ac.uk>
2025-03-07 15:34:59 +0000

    Ship Generation should never get stuck
    In theory

--- BattleShips/Board.cs
+++ BattleShips/Board.cs
@@ -79,6 +79,24 @@ namespace BattleShips
             return false;
         }
 
+        private List<Vector2> AllSpaces
+        {
+            get
+            {
+                //create a list with all available spaces
+                List<Vector2> available = new List<Vector2>();
+
+                for (int y = 0; y < _spaces.GetLength(0); y++)
+                {
+                    for (int x = 0; x < _spaces.GetLength(1); x++)
+                    {
+                        available.Add(new Vector2(x, y));
+                    }
+                }
+                return available;
+            }
+        }
+
         public void GenerateShips(List<int[]> ships) //ship == {3, 4} - meaning 3 ships of size 4
         {
             //calculate ship spaces - could be done while changing spaces, but it has a miniscule impact on performence
@@ -86,90 +104,101 @@ namespace BattleShips
                 _shipSpaces += ship[0] * ship[1];
 
             //create a list with all available spaces
-            List<int[]> available = new List<int[]>();
-
-            for (int y = 0; y < _spaces.GetLength(0); y++)
-            {
-                for (int x = 0; x < _spaces.GetLength(1); x++)
-                {
-                    available.Add([y, x]);
-                }
-            }
+            List<Vector2> available = AllSpaces;
 
             //loop through all the ships to add
             for (int shipType = 0; shipType < ships.Count(); shipType++)
             {
                 for (int count = 0; count < ships[shipType][0]; count++)
                 {
-                    //create a counter to keep track of how many times the ship tried to place
-                    int counter = 0;
+                    //check if any pos is valid
+                    var currentlyAvailable = new List<Vector2>(available);
                     while (true)
                     {
-                        //increase counter
-                        counter++;
+                        //if there are no valid positions, wipe slate clean
+                        if (currentlyAvailable.Count() == 0)
+                        {
+                            shipType = 0;
+                            count = -1;
+                            _spaces = new SpaceStates[Height, Width];
+                            available = AllSpaces;
+                            break;
+                        }
 
                         //get random position
-                        int availableInd = Program.RNG.Next(available.Count());
-                        int y = available[availableInd][0];
-                        int x = available[availableInd][1];
+                        int availableInd = Program.RNG.Next(currentlyAvailable.Count());
+                        int y = currentlyAvailable[availableInd].y;
+                        int x = currentlyAvailable[availableInd].x;
 
                         //randomize whether ship is vertical or horizontal 
                         bool vertical = false;
                         if (Program.RNG.Next(2) == 1)
                             vertical = true;
 
-                        //if ships would run off the board, get another position
-                        if (vertical)
+                        //check if either vertical or non vertical is valid
+                        bool valid = false;
+                        for (int i = 0; i < 2; i++)
                         {
-                            if (y + ships[shipType][1]-1 >= _spaces.GetLength(0))
-                            {
-                                continue;
-                            }
-                        }
-                        else
-                        {
-                            if (x + ships[shipType][1]-1 >= _spaces.GetLength(1))
-                            {
-                                continue;
-                            }
-                        }
+                            //check if this pos will work flipped
+                            if (i == 1)
+                                vertical = !vertical;
 
-                        //check if all the spaces are not already taken up
-                        bool valid = true;
-                        for (int i = 0; i < ships[shipType][1]; i++)
-                        {
-                            //check for vertical spaces
+                            //if ships would run off the board, get another position
                             if (vertical)
                             {
-                                if (_spaces[y + i, x] != SpaceStates.empty)
+                                if (y + ships[shipType][1] - 1 >= Height)
+                                    continue;
+                            }
+                            else
+                            {
+                                if (x + ships[shipType][1] - 1 >= Width)
+                                    continue;
+                            }
+
+                            //check if all the spaces are not already taken up
+                            valid = true;
+                            for (int j = 0; j < ships[shipType][1]; j++)
+                            {
+                                //check for vertical spaces
+                                if (vertical)
+                                {
+                                    if (_spaces[y + j, x] != SpaceStates.empty)
+                                    {
+                                        valid = false;
+                                        break;
+                                    }
+                                }
+                                //otherwise check the horizontal spaces
+                                else if (_spaces[y, x + j] != SpaceStates.empty)
                                 {
                                     valid = false;
                                     break;
                                 }
-                                continue;
                             }
-                            //otherwise check the horizontal spaces
-                            if (_spaces[y, x + i] != SpaceStates.empty)
-                            {
-                                valid = false;
+                            if (valid)
                                 break;
-                            }
                         }
-                        //if wasnt valid, grab another position
+
+                        //if isn't valid remove from available spaces and choose a different pos
                         if (!valid)
+                        {
+                            currentlyAvailable.RemoveAt(availableInd);
                             continue;
+                        }
 
                         //add to board
                         for (int i = 0; i < ships[shipType][1]; i++)
                         {
+                            int curX = x;
+                            int curY = y;
+
                             if (vertical)
-                            {
-                                _spaces[y + i, x] = SpaceStates.ship;
-                                available.Remove([y + i, x]);
-                                continue;
-                            }
-                            _spaces[y, x + i] = SpaceStates.ship;
-                            available.Remove([y, x + i]);
+                                curY += i;
+                            else
+                                curX += i;
+
+                            _spaces[curY, curX] = SpaceStates.ship;
+                            available.Remove(new Vector2(curX, curY));
                         }
                         break;
                     }
